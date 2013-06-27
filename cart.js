30d5e40a75f849e81d94ef3eab86cc3a02d498be
Ionica Bizau <bizauionica@yahoo.com>
2013-06-27 12:31:11 +0300

    Use binds instead of jQuery selectors.

--- cart.js
+++ cart.js
@@ -410,10 +410,15 @@ function Cart(module) {
 
                     if (err) { showError(err); }
 
-                    $("#subtotal", self.dom).text(toPriceString(costs.subtotal));
-                    $("#vat", self.dom).text(toPriceString(costs.vat));
-                    $("#ship", self.dom).text(toPriceString(costs.ship));
-                    $("#total", self.dom).text(toPriceString(costs.total));
+                    var binds = config.oncompute.total;
+                    for (var i in binds) {
+                        Bind.call(self, binds[i], costs);
+                    }
+
+                    // $("#subtotal", self.dom).text(toPriceString(costs.subtotal));
+                    // $("#vat", self.dom).text(toPriceString(costs.vat));
+                    // $("#ship", self.dom).text(toPriceString(costs.ship));
+                    // $("#total", self.dom).text(toPriceString(costs.total));
                 });
                 break
 
